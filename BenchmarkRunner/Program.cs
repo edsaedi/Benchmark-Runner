f484ff5cf23c4e03278c7df8c733141e9eb8fae1
Edan Saedi <Edan.Saedi@GMR.local>
2020-10-07 18:18:41 -0700

    Finished

--- BenchmarkRunner/Program.cs
+++ BenchmarkRunner/Program.cs
@@ -2,16 +2,17 @@
 using System.Collections.Generic;
 using System.Reflection;
 using System.Text;
+using System.Diagnostics;
 
 namespace BenchmarkRunner
 {
     [AttributeUsage(AttributeTargets.Method)]
-    class BenchmarkAttribute : Attribute
+    public class BenchmarkAttribute : Attribute
     {
 
     }
 
-    class BenchmarkRunner
+    public class BenchmarkRunner
     {
         public static void Run<T>()
         {
@@ -21,6 +22,7 @@ namespace BenchmarkRunner
             var methods = tType.GetMethods();
             //Method and Mean of functions to print out.
 
+
             foreach (var method in methods)
             {
                 var methodAttributes = method.GetCustomAttributes();
@@ -29,48 +31,65 @@ namespace BenchmarkRunner
                 {
                     if (methodAttribute is BenchmarkAttribute)
                     {
-                        method.Invoke(null, null);
+                        for (int i = 0; i < 3; i++)
+                        {
+                            method.Invoke(null, null);
+                        }
+                        
+                        double totalElapsedTime = 0;
+                        for (int i = 0; i < 3; i++)
+                        {
+                            var stopwatch = Stopwatch.StartNew();
+                            method.Invoke(null, null);
+                            stopwatch.Stop();
+                            totalElapsedTime += stopwatch.Elapsed.TotalMilliseconds;
+                            Console.WriteLine($"{method.Name}: Elapsed={stopwatch.Elapsed.TotalMilliseconds}");
+                        }
+
+                        Console.WriteLine($"{method.Name}: Average Time={totalElapsedTime / 3}");
                     }
                 }
+
                 //Finish the function. Print out method and the mean of the functions.
+
             }
         }
     }
 
-    class BenchmarkTests
-    {
-        const int size = 100000;
+    //class BenchmarkTests
+    //{
+    //    const int size = 100000;
 
-        [Benchmark]
-        public void StringConcat()
-        {
-            string str = "";
+    //    [Benchmark]
+    //    public static void StringConcat()
+    //    {
+    //        string str = "";
 
-            for (int i = 0; i < size; i++)
-            {
-                str += "A";
-            }
-        }
+    //        for (int i = 0; i < size; i++)
+    //        {
+    //            str += "A";
+    //        }
+    //    }
 
-        [Benchmark]
-        public void StringBuilderAppend()
-        {
-            StringBuilder stringBuilder = new StringBuilder();
+    //    [Benchmark]
+    //    public static void StringBuilderAppend()
+    //    {
+    //        StringBuilder stringBuilder = new StringBuilder();
 
-            for (int i = 0; i < size; i++)
-            {
-                stringBuilder.Append("A");
-            }
+    //        for (int i = 0; i < size; i++)
+    //        {
+    //            stringBuilder.Append("A");
+    //        }
 
-        }
-    }
+    //    }
+    //}
 
 
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            BenchmarkRunner.Run<BenchmarkTests>();
-        }
-    }
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        BenchmarkRunner.Run<BenchmarkTests>();
+    //    }
+    //}
 }
